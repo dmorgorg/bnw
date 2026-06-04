484f32848866d8cb84219c5212d481240ec4f737
Dave Morgan <dave@dmorg.org>
2026-06-03 19:06:41 -0600

    first commit

---
+++ src/routes/albums/2012india/+page.svelte
@@ -0,0 +1,208 @@
+<script>
+	import Reveal from 'reveal.js';
+	import { onMount } from 'svelte';
+
+	import 'reveal.js/reveal.css';
+	import 'reveal.js/theme/white.css';
+	import '$lib/app.scss';
+
+	onMount(() => {
+		const deck = new Reveal({
+			autoAnimateEasing: 'ease',
+			autoAnimateDuration: 1,
+			hash: true, // for url hash
+			controls: true, // true by default
+			progress: true // true by default
+		});
+		deck.initialize();
+	});
+</script>
+
+<div class="reveal">
+	<div class="slides">
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-11-115811_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-11-115811.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-11-121801_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-11-121801.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-14-130650_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-14-130650.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-14-130825_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-14-130825.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-19-060801_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-19-060801-1.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-19-091019_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-19-091019.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-21-114512_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-21-114512.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-21-123345_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-21-123345.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-105155_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-105155.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-112647_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-112647.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-121330_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-121330.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-125843_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-125843.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-130608_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-130608.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-132700_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-22-132700.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-23-091755_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-23-091755.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-131811_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-131811.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-133018_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-133018.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-133656_Nik.jpg" alt="" />
+			</section>
+			<section>
+				<img src="/images/2012india/09/2012-09-28-133656.jpg" alt="" />
+			</section>
+		</section>
+
+		<section>
+			<img src="/images/2012india/2012-10-06-121557_Nik.jpg" alt="" />
+			<!-- Monastery at -->
+		</section>
+		<section>
+			<img src="/images/2012india/2012-10-06-121940_Nik.jpg" alt="" />
+		</section>
+		<section>
+			<img src="/images/2012india/2012-10-09-115350_Nik.jpg" alt="" />
+		</section>
+		<section>
+			<img src="/images/2012india/2012-10-14-162901_Nik.jpg" alt="" />
+		</section>
+		<section>
+			<img src="/images/2012india/2012-10-14-162952_Nik.jpg" alt="" />
+		</section>
+		<section>
+			<img src="/images/2012india/2012-10-18-104950_Nik.jpg" alt="" />
+		</section>
+		<section>
+			<img src="/images/2012india/2012-11-04-130636_Nik.jpg" alt="" />
+		</section>
+	</div>
+</div>
