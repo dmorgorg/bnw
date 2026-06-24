157bb9fbc6451f8dce80674d4a0ed602b98bfe38
Dave Morgan <dave@dmorg.org>
2026-06-24 16:00:44 -0600

    2018 US

--- src/routes/albums/2018US/+page.svelte
+++ src/routes/albums/2018US/+page.svelte
@@ -2,8 +2,25 @@
 	import AlbumSlider from '$lib/components/AlbumSlider.svelte';
 	import NavigationController from '$lib/components/NavigationController.svelte';
 	import '$lib/app.scss';
-	import bw01 from '../../../lib/images/2012UK/2012-08-07-184627_Nik.jpg?enhanced';
-	import c01 from '../../../lib/images/2012UK/2012-08-07-184627.jpg?enhanced';
+
+	import bw01 from '../../../lib/images/2018US/2018-12-02-155643_Nik.jpg?enhanced';
+	import c01 from '../../../lib/images/2018US/2018-12-02-155643.jpg?enhanced';
+	import bw02 from '../../../lib/images/2018US/2018-12-03-120300_Nik.jpg?enhanced';
+	import c02 from '../../../lib/images/2018US/2018-12-03-120300.jpg?enhanced';
+	import bw03 from '../../../lib/images/2018US/2018-12-03-125743_Nik.jpg?enhanced';
+	import c03 from '../../../lib/images/2018US/2018-12-03-125743.jpg?enhanced';
+	import bw04 from '../../../lib/images/2018US/2018-12-04-153901_Nik.jpg?enhanced';
+	import c04 from '../../../lib/images/2018US/2018-12-04-153901.jpg?enhanced';
+	import bw05 from '../../../lib/images/2018US/2018-12-13-154106_Nik-1.jpg?enhanced';
+	import c05 from '../../../lib/images/2018US/2018-12-13-154106.jpg?enhanced';
+	import bw06 from '../../../lib/images/2018US/2018-12-13-164525_Nik.jpg?enhanced';
+	import c06 from '../../../lib/images/2018US/2018-12-13-164525.jpg?enhanced';
+	import bw07 from '../../../lib/images/2018US/2018-12-13-165843_Nik.jpg?enhanced';
+	import c07 from '../../../lib/images/2018US/2018-12-13-165843.jpg?enhanced';
+	import bw08 from '../../../lib/images/2018US/2018-12-13-172307_Nik.jpg?enhanced';
+	import c08 from '../../../lib/images/2018US/2018-12-13-172307.jpg?enhanced';
+	import bw09 from '../../../lib/images/2018US/2018-12-15-130409_Nik.jpg?enhanced';
+	import c09 from '../../../lib/images/2018US/2018-12-15-130409.jpg?enhanced';
 
 	let hIndex = $state(0);
 	let vIndex = $state(0);
@@ -12,9 +29,72 @@
 		[
 			{
 				image: bw01,
-				caption: 'Exposing the kids to my old stomping grounds. Bosigran, Cornwall. August, 2012'
+				caption: 'After the storm, Utah. December, 2018'
 			},
 			{ image: c01, caption: ' ' }
+		],
+		[
+			{
+				image: bw02,
+				caption: 'After the storm, Utah. December, 2018'
+			},
+			{ image: c02, caption: ' ' }
+		],
+		[
+			{
+				image: bw03,
+				caption: 'After the storm, Utah. December, 2018'
+			},
+			{ image: c03, caption: ' ' }
+		],
+		[
+			{
+				image: bw04,
+				caption: 'Arizona. December, 2018'
+			},
+			{ image: c04, caption: ' ' }
+		],
+		[
+			{
+				image: bw05,
+				caption: 'Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw05, caption: ' ' }
+		],
+		[
+			{
+				image: bw06,
+				caption: 'Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw06, caption: ' ' }
+		],
+		[
+			{
+				image: bw07,
+				caption: 'Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw07, caption: ' ' }
+		],
+		[
+			{
+				image: bw07,
+				caption: 'Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw07, caption: ' ' }
+		],
+		[
+			{
+				image: bw08,
+				caption: 'Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw08, caption: ' ' }
+		],
+		[
+			{
+				image: bw09,
+				caption: 'Hidden Valley, Joshua Tree National Park, California. December, 2018'
+			},
+			{ image: bw09, caption: ' ' }
 		]
 	];
 </script>
