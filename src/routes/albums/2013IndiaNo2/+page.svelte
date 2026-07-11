edad09ecf7505aea3727016830a587edf6da4427
Dave Morgan <dave@dmorg.org>
2026-07-10 20:09:08 -0600

    on the road to kargil

--- src/routes/albums/2013IndiaNo2/+page.svelte
+++ src/routes/albums/2013IndiaNo2/+page.svelte
@@ -17,6 +17,18 @@
 	import c06 from '../../../lib/images/2013India/2013-06-05-115614.jpg?enhanced';
 	import bw07 from '../../../lib/images/2013India/2013-06-05-115810_Nik.jpg?enhanced';
 	import c07 from '../../../lib/images/2013India/2013-06-05-115810.jpg?enhanced';
+	import bw08 from '../../../lib/images/2013India/2013-06-06-111753_Nik.jpg?enhanced';
+	import c08 from '../../../lib/images/2013India/2013-06-06-111753.jpg?enhanced';
+	import bw09 from '../../../lib/images/2013India/2013-06-06-112629_Nik.jpg?enhanced';
+	import c09 from '../../../lib/images/2013India/2013-06-06-112629.jpg?enhanced';
+	import bw10 from '../../../lib/images/2013India/2013-06-06-114218_Nik.jpg?enhanced';
+	import c10 from '../../../lib/images/2013India/2013-06-06-114218.jpg?enhanced';
+	import bw11 from '../../../lib/images/2013India/2013-06-06-114816_Nik.jpg?enhanced';
+	import c11 from '../../../lib/images/2013India/2013-06-06-114816.jpg?enhanced';
+	import bw12 from '../../../lib/images/2013India/2013-06-06-141248_Nik.jpg?enhanced';
+	import c12 from '../../../lib/images/2013India/2013-06-06-141248.jpg?enhanced';
+	import bw13 from '../../../lib/images/2013India/2013-06-06-170821_Nik.jpg?enhanced';
+	import c13 from '../../../lib/images/2013India/2013-06-06-170821.jpg?enhanced';
 
 	let hIndex = $state(0);
 	let vIndex = $state(0);
@@ -70,6 +82,51 @@
 				caption: 'Srinagar, Jammu and Kashmir. June, 2013'
 			},
 			{ image: c07, caption: ' ' }
+		],
+		[
+			{
+				image: bw08,
+				caption:
+					'The landscapes from the NH1, from Srinagar towards Leh, are spectacular. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c08, caption: ' ' }
+		],
+		[
+			{
+				image: bw09,
+				caption: 'A shepherd at the roadside whistles to his flock. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c09, caption: ' ' }
+		],
+		[
+			{
+				image: bw10,
+				caption:
+					'The NH1 is slow going. On the road very early and it was still getting dark as we approached Kargil, little more than 200 km from Srinagar. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c10, caption: ' ' }
+		],
+		[
+			{
+				image: bw11,
+				caption:
+					'Nose to tail traffic on the NH1 from Srinagar towards Leh. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c11, caption: ' ' }
+		],
+		[
+			{
+				image: bw12,
+				caption: 'Between Srinagar and Kargil. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c12, caption: ' ' }
+		],
+		[
+			{
+				image: bw13,
+				caption: 'Between Srinagar and Kargil. Jammu and Kashmir. June, 2013'
+			},
+			{ image: c13, caption: ' ' }
 		],
 		[
 			{
