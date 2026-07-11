87ca850ada74b1e3fd69af338274f12bca1f0dee
Dave Morgan <dave@dmorg.org>
2026-07-11 01:13:48 -0600

    latest

--- src/lib/components/AlbumSlider.svelte
+++ src/lib/components/AlbumSlider.svelte
@@ -1,6 +1,35 @@
 <script>
 	import { fade } from 'svelte/transition';
 	let { hIndex, vIndex, imagesArray } = $props();
+	let mediaEl;
+	let captionWidth = $state('90%');
+
+	const setCaptionWidth = () => {
+		const img = mediaEl?.querySelector('img');
+		if (!img) return;
+		const measured = Math.round(img.getBoundingClientRect().width * 0.9);
+		captionWidth = measured > 0 ? `${measured}px` : '90%';
+	};
+
+	$effect(() => {
+		hIndex;
+		vIndex;
+		setCaptionWidth();
+	});
+
+	$effect(() => {
+		const img = mediaEl?.querySelector('img');
+		if (!img) return;
+
+		const observer = new ResizeObserver(() => {
+			setCaptionWidth();
+		});
+
+		observer.observe(img);
+		setCaptionWidth();
+
+		return () => observer.disconnect();
+	});
 
 	// svelte-ignore state_referenced_locally
 	// let numberOfSlides = imagesArray.length;
@@ -9,14 +38,13 @@
 
 {#key `${hIndex}-${vIndex}`}
 	<div class="wrapper" out:fade={{ duration: 500 }} in:fade={{ duration: 1000, delay: 600 }}>
-		<div class="media">
+		<div class="media" bind:this={mediaEl}>
 			<enhanced:img
 				src={imagesArray[Number(hIndex)][Number(vIndex)].image}
 				sizes="(min-width:1920px) 1800px, (min-width:1080px) 1080px, (min-width:768px) 768px"
 				alt=""
 			/>
-
-			<div class="caption">
+			<div class="caption" style:width={captionWidth}>
 				<strong>Image {hIndex + 1}/{imagesArray.length}. </strong> &nbsp;
 				{@html imagesArray[Number(hIndex)][0].caption}
 			</div>
@@ -55,9 +83,9 @@
 	}
 	.caption {
 		font-family: 'captionText';
-		height: 2em;
+		line-height: 1.2;
 		margin-top: 0.5em;
 		text-align: center;
-		width: 90%;
+		max-width: 100%;
 	}
 </style>
