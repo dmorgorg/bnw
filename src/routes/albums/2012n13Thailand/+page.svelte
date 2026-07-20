adc6122f6e2b101b14e7a7fc4e23334f44bd0980
Dave Morgan <dave@dmorg.org>
2026-07-19 18:27:19 -0600

    bangkok

--- src/routes/albums/2012n13Thailand/+page.svelte
+++ src/routes/albums/2012n13Thailand/+page.svelte
@@ -16,15 +16,29 @@
 	import c06 from '../../../lib/images/2012Thailand/2012-09-05-103516-PR.jpg?enhanced';
 	import bw07 from '../../../lib/images/2012Thailand/2012-09-05-111556-PR_Nik.jpg?enhanced';
 	import c07 from '../../../lib/images/2012Thailand/2012-09-05-111556-PR.jpg?enhanced';
-	import bw08 from '../../../lib/images/2012Thailand/2012-09-05-114007_Nik.jpg?enhanced';
-	import c08 from '../../../lib/images/2012Thailand/2012-09-05-114007.jpg?enhanced';
+	import bw08 from '../../../lib/images/2012Thailand/2012-09-05-111652-PR_Nik.jpg?enhanced';
+	import c08 from '../../../lib/images/2012Thailand/2012-09-05-111652-PR.jpg?enhanced';
+	import bw09 from '../../../lib/images/2012Thailand/2012-09-05-114007_Nik.jpg?enhanced';
+	import c09 from '../../../lib/images/2012Thailand/2012-09-05-114007.jpg?enhanced';
+	import bw10 from '../../../lib/images/2012Thailand/2012-09-06-082050_Nik.jpg?enhanced';
+	import c10 from '../../../lib/images/2012Thailand/2012-09-06-082050.jpg?enhanced';
+	import bw11 from '../../../lib/images/2012Thailand/2012-09-06-085129_Nik.jpg?enhanced';
+	import c11 from '../../../lib/images/2012Thailand/2012-09-06-085129.jpg?enhanced';
+	import bw12 from '../../../lib/images/2012Thailand/2012-09-06-085421_Nik.jpg?enhanced';
+	import c12 from '../../../lib/images/2012Thailand/2012-09-06-085421.jpg?enhanced';
+	import bw13 from '../../../lib/images/2012Thailand/2012-09-06-091908_Nik.jpg?enhanced';
+	import c13 from '../../../lib/images/2012Thailand/2012-09-06-091908.jpg?enhanced';
+	import bw14 from '../../../lib/images/2012Thailand/2012-09-06-102806_Nik.jpg?enhanced';
+	import c14 from '../../../lib/images/2012Thailand/2012-09-06-102806.jpg?enhanced';
+	import bw15 from '../../../lib/images/2012Thailand/2012-09-06-103107_Nik.jpg?enhanced';
+	import c15 from '../../../lib/images/2012Thailand/2012-09-06-103107.jpg?enhanced';
 
-	import bw101 from '../../../lib/images/2013Thailand/2013-05-09-053742_Nik.jpg?enhanced';
-	import c101 from '../../../lib/images/2013Thailand/2013-05-09-053742.jpg?enhanced';
-	import bw102 from '../../../lib/images/2013Thailand/2013-05-09-053901_Nik.jpg?enhanced';
-	import c102 from '../../../lib/images/2013Thailand/2013-05-09-053901.jpg?enhanced';
-	import bw103 from '../../../lib/images/2013Thailand/2013-05-09-053918_Nik.jpg?enhanced';
-	import c103 from '../../../lib/images/2013Thailand/2013-05-09-053918.jpg?enhanced';
+	import bw091 from '../../../lib/images/2013Thailand/2013-05-09-053742_Nik.jpg?enhanced';
+	import c091 from '../../../lib/images/2013Thailand/2013-05-09-053742.jpg?enhanced';
+	import bw092 from '../../../lib/images/2013Thailand/2013-05-09-053901_Nik.jpg?enhanced';
+	import c092 from '../../../lib/images/2013Thailand/2013-05-09-053901.jpg?enhanced';
+	import bw093 from '../../../lib/images/2013Thailand/2013-05-09-053918_Nik.jpg?enhanced';
+	import c093 from '../../../lib/images/2013Thailand/2013-05-09-053918.jpg?enhanced';
 
 	let hIndex = $state(0);
 	let vIndex = $state(0);
@@ -40,7 +54,7 @@
 		[
 			{
 				image: bw02,
-				caption: 'Rare quiet Bangkok streets at night. August, 2012'
+				caption: 'A rare quiet Bangkok street at night. August, 2012'
 			},
 			{ image: c02, caption: ' ' }
 		],
@@ -68,67 +82,97 @@
 		[
 			{
 				image: bw06,
-				caption: 'Waiting at the lights. Thonburi, Bangkok. September, 2012'
+				caption:
+					'Waiting at the lights. Thonburi is far enough from the centre of bustling Bangkok that cycle rickshaws still prevail. September, 2012'
 			},
 			{ image: c06, caption: ' ' }
 		],
 		[
 			{
-				image: bw08,
+				image: bw07,
 				caption: 'Thonburi, Bangkok. September, 2012'
 			},
-			{ image: c08, caption: ' ' }
+			{ image: c07, caption: ' ' }
 		],
 
 		[
 			{
-				image: bw101,
-				caption: 'Prachuap Khiri Khan. May, 2013'
+				image: bw09,
+				caption: 'Thonburi, Bangkok. September, 2012'
 			},
-			{ image: c101, caption: ' ' }
+			{ image: c08, caption: ' ' }
 		],
 		[
 			{
-				image: bw102,
-				caption: 'Prachuap Khiri Khan. May, 2013'
+				image: bw09,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
 			},
-			{ image: c102, caption: ' ' }
+			{ image: c09, caption: ' ' }
 		],
 		[
 			{
-				image: bw103,
+				image: bw10,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c10, caption: ' ' }
+		],
+		[
+			{
+				image: bw11,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c11, caption: ' ' }
+		],
+		[
+			{
+				image: bw12,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c12, caption: ' ' }
+		],
+		[
+			{
+				image: bw13,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c13, caption: ' ' }
+		],
+		[
+			{
+				image: bw14,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c14, caption: ' ' }
+		],
+		[
+			{
+				image: bw15,
+				caption: 'Wat Phra Kaew, Bangkok. September, 2012'
+			},
+			{ image: c15, caption: ' ' }
+		],
+
+		[
+			{
+				image: bw091,
 				caption: 'Prachuap Khiri Khan. May, 2013'
 			},
-			{ image: c103, caption: ' ' }
+			{ image: c091, caption: ' ' }
+		],
+		[
+			{
+				image: bw092,
+				caption: 'Prachuap Khiri Khan. May, 2013'
+			},
+			{ image: c092, caption: ' ' }
+		],
+		[
+			{
+				image: bw093,
+				caption: 'Prachuap Khiri Khan. May, 2013'
+			},
+			{ image: c093, caption: ' ' }
 		]
-		// [
-		// 	{
-		// 		image: bw04,
-		// 		caption: 'Simla. May, 2013'
-		// 	},
-		// 	{ image: c04, caption: ' ' }
-		// ],
-		// [
-		// 	{
-		// 		image: bw05,
-		// 		caption: 'Simla. May, 2013'
-		// 	},
-		// 	{ image: c05, caption: ' ' }
-		// ],
-		// [
-		// 	{
-		// 		image: bw06,
-		// 		caption: 'Simla. May, 2013'
-		// 	},
-		// 	{ image: c06, caption: ' ' }
-		// ],
-		// [
-		// 	{
-		// 		image: bw07,
-		// 		caption: 'Triund, above Dharamsala. May, 2013'
-		// 	},
-		// 	{ image: c07, caption: ' ' }
-		// ]
 	];
 </script>
 
