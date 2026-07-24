2974a743a6445ed6799d731052b7c120f83408ca
Dave Morgan <dave@dmorg.org>
2026-07-24 14:10:06 -0600

    some color birds

---
+++ src/routes/birds/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { page } from '$app/state';
+	import favicon from '$lib/assets/camera.png';
+	import HomeIcon from '$lib/components/HomeIcon.svelte';
+	import AlbumsDropDown from '$lib/components/AlbumsDropDown.svelte';
+	import { fade } from 'svelte/transition';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="outer">
+	<div class="home-icon-wrapper">
+		<HomeIcon fontSize="clamp(10px, 1.2vw, 20px)" />
+	</div>
+	<AlbumsDropDown />
+
+	<div class="content-stage">
+		{#key page.url.pathname}
+			<div
+				class="content-panel"
+				out:fade={{ duration: 500 }}
+				in:fade={{ duration: 1000, delay: 500 }}
+			>
+				{@render children()}
+			</div>
+		{/key}
+	</div>
+</div>
+
+<style lang="scss">
+	.outer {
+		// border: 1px solid white; // not sure why i need this...to get correct height
+		height: 100vh;
+		max-height: 100vh;
+		overflow: hidden;
+		position: relative;
+		padding: 0;
+	}
+	.content-stage {
+		height: 100%;
+		overflow: hidden;
+		position: relative;
+		z-index: 1;
+	}
+	.content-panel {
+		inset: 0;
+		position: absolute;
+	}
+	.home-icon-wrapper {
+		position: absolute;
+		left: clamp(5px, 0.5vw, 15px);
+		top: clamp(5px, 0.5vw, 15px);
+		z-index: 3;
+
+		&:hover {
+			box-shadow: 0 0 8px 2px #aaa;
+		}
+	}
+</style>
---
+++ src/routes/birds/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import AlbumSlider from '$lib/components/AlbumSlider.svelte';
+	import NavigationController from '$lib/components/NavigationController.svelte';
+	import '$lib/app.scss';
+
+	// american coot
+	import b005 from '../../lib/images/birds/2026-07-13-144412.jpg?enhanced';
+	import b006 from '../../lib/images/birds/2026-07-13-144243.jpg?enhanced';
+
+	// northern flicker
+	import b001 from '../../lib/images/birds/2026-07-09-181359.jpg?enhanced';
+	import b002 from '../../lib/images/birds/2026-07-09-181422.jpg?enhanced';
+
+	// ruddy duck
+	import b003 from '../../lib/images/birds/2026-07-13-165427.jpg?enhanced';
+
+	// white-faced ibis
+	import b004 from '../../lib/images/birds/2026-07-13-153349.jpg?enhanced';
+
+	let hIndex = $state(0);
+	let vIndex = $state(0);
+
+	const imagesArray = [
+		[
+			{
+				image: b005,
+				caption: 'American Coot, Frank Lake, July 2026'
+			},
+			{
+				image: b006,
+				caption: 'American Coot, Frank Lake, July 2026'
+			}
+		],
+		[
+			{
+				image: b001,
+				caption: 'Northern Flicker, Calgary, July 2026'
+			},
+			{ image: b002, caption: 'Northern Flicker, Calgary, July 2026' },
+			{ image: b001, caption: ' ' }
+		],
+		[
+			{
+				image: b003,
+				caption: 'Ruddy Duck, Frank Lake, July 2026'
+			}
+		],
+		[
+			{
+				image: b004,
+				caption: 'White-faced ibis, Frank Lake, July 2026'
+			}
+		]
+	];
+</script>
+
+<div class="albumTitle">This Album: Birds - colour</div>
+
+<div class="sliderWrapper">
+	<NavigationController bind:hIndex bind:vIndex {imagesArray} />
+	<AlbumSlider bind:hIndex bind:vIndex {imagesArray} />
+</div>
