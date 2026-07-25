42715eda179e5ed275e9d08cb48a448cef01de9c
Dave Morgan <dave@dmorg.org>
2026-07-24 19:23:39 -0600

    birds

--- src/routes/birds/+page.svelte
+++ src/routes/birds/+page.svelte
@@ -1,21 +1,28 @@
 <script>
-	import AlbumSlider from '$lib/components/AlbumSlider.svelte';
+	import AlbumSlider2 from '$lib/components/AlbumSlider2.svelte';
 	import NavigationController from '$lib/components/NavigationController.svelte';
 	import '$lib/app.scss';
 
 	// american coot
 	import b005 from '../../lib/images/birds/2026-07-13-144412.jpg?enhanced';
 	import b006 from '../../lib/images/birds/2026-07-13-144243.jpg?enhanced';
+	import b007 from '../../lib/images/birds/2026-07-13-144252.jpg?enhanced';
+	import b008 from '../../lib/images/birds/2026-07-13-144457.jpg?enhanced';
+	import b009 from '../../lib/images/birds/2026-07-13-144541.jpg?enhanced';
 
 	// northern flicker
 	import b001 from '../../lib/images/birds/2026-07-09-181359.jpg?enhanced';
 	import b002 from '../../lib/images/birds/2026-07-09-181422.jpg?enhanced';
+	import b010 from '../../lib/images/birds/2026-07-09-181605.jpg?enhanced';
 
 	// ruddy duck
 	import b003 from '../../lib/images/birds/2026-07-13-165427.jpg?enhanced';
 
 	// white-faced ibis
 	import b004 from '../../lib/images/birds/2026-07-13-153349.jpg?enhanced';
+
+	// yellow-headed blackbird
+	import b011 from '../../lib/images/birds/2026-07-13-145838.jpg?enhanced';
 
 	let hIndex = $state(0);
 	let vIndex = $state(0);
@@ -24,31 +31,50 @@
 		[
 			{
 				image: b005,
-				caption: 'American Coot, Frank Lake, July 2026'
+				caption:
+					'Adult <strong>American Coot</strong>. <br/>The coot is not a duck, and does not have webbed feet. <br/>Frank Lake, Alberta. July 2026'
+			},
+			{
+				image: b008,
+				caption: 'American Coot chick. <br/> Frank Lake, Alberta. July 2026'
+			},
+			{
+				image: b009,
+				caption: 'American Coot and juvenile. <br/>Frank Lake, Alberta. July 2026'
 			},
 			{
 				image: b006,
-				caption: 'American Coot, Frank Lake, July 2026'
+				caption: 'American Coot and juvenile. <br/>Frank Lake, Alberta. July 2026'
+			},
+			{
+				image: b007,
+				caption: 'Juvenile American Coot. <br/> Frank Lake, Alberta. July 2026'
 			}
 		],
 		[
 			{
 				image: b001,
-				caption: 'Northern Flicker, Calgary, July 2026'
+				caption: '<strong>Northern Flicker</strong>.<br/> Calgary, Alberta. July 2026'
 			},
-			{ image: b002, caption: 'Northern Flicker, Calgary, July 2026' },
-			{ image: b001, caption: ' ' }
+			{ image: b002, caption: 'Northern Flicker. <br/> Calgary, Alberta. July 2026' },
+			{ image: b010, caption: 'Northern Flicker. <br/> Calgary, Alberta. July 2026' }
 		],
 		[
 			{
 				image: b003,
-				caption: 'Ruddy Duck, Frank Lake, July 2026'
+				caption: '<strong>Ruddy Duck</strong>. <br/> Frank Lake, Alberta. July 2026'
 			}
 		],
 		[
 			{
 				image: b004,
-				caption: 'White-faced ibis, Frank Lake, July 2026'
+				caption: '<strong>White-faced Ibis</strong>. <br/>Frank Lake, Alberta. July 2026'
+			}
+		],
+		[
+			{
+				image: b011,
+				caption: '<strong>Yellow-headed Blackbird</strong>, <br/>Frank Lake, Alberta. July 2026'
 			}
 		]
 	];
@@ -58,5 +84,5 @@
 
 <div class="sliderWrapper">
 	<NavigationController bind:hIndex bind:vIndex {imagesArray} />
-	<AlbumSlider bind:hIndex bind:vIndex {imagesArray} />
+	<AlbumSlider2 bind:hIndex bind:vIndex {imagesArray} label="Bird species" />
 </div>
